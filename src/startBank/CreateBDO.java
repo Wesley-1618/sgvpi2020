5a50f8a5d044e6c1e4c45feb5f555d333997a29e
ocult <ocult@AkawsyDesk>
2020-11-08 23:20:14 -0300

    All Tables Creates (testar)

--- src/startBank/CreateBDO.java
+++ src/startBank/CreateBDO.java
@@ -19,6 +19,12 @@ public class CreateBDO {
 		criarTipoProduto(tabConect);
 		criarProduto(tabConect);
 		criarFornecedor(tabConect);
+		criarFornecimento(tabConect);
+		criarEstoque(tabConect);
+		criarCliente(tabConect);
+		criarEnderecoCliente(tabConect);
+		criarFormaPagamento(tabConect);
+		criarVenda(tabConect);
 	}
 
 	private void criarLoja(Connection link) {
@@ -104,7 +110,7 @@ public class CreateBDO {
 		sql.append("nome_fornecedor varchar (40) not null, ");
 		sql.append("cnpj_fornecedor numeric(14) not null,");
 		sql.append("contato_fornecedor varchar(40),");
-		sql.append("tel_fornecedor numeric (12),");
+		sql.append("telefone_fornecedor numeric (12),");
 		sql.append("constraint fornecedor_pkey primary key (cod_fornecedor),");
 		sql.append("foreign key (cod_produto) References Produto (cod_produto) ");
 		sql.append(")");
@@ -115,5 +121,129 @@ public class CreateBDO {
 			ConnectPg.printSQLException(e, null);
 		}
 
+	}
+
+	private void criarFornecimento(Connection link) {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("CREATE TABLE IF NOT EXISTS Fornecimento");
+		sql.append("(");
+		sql.append("cod_fornecimento integer not null,");
+		sql.append("cod_loja integer not null, ");
+		sql.append("cod_fornecedor integer not null, ");
+		sql.append("data_fornecimento date not null,");
+		sql.append("constraint fornecimento_pkey primary key (cod_fornecimento),");
+		sql.append("foreign key (cod_loja) references Loja (cod_loja)");
+		sql.append("foreign key (cod_fornecedor) references Fornecedor (cod_fornecedor)");
+		sql.append(")");
+
+		try (Statement comando = link.createStatement();) {
+			comando.execute(sql.toString());
+		} catch (SQLException e) {
+			ConnectPg.printSQLException(e, null);
+		}
+	}
+
+	private void criarEstoque(Connection link) {
+		StringBuilder sql = new StringBuilder();
+		sql.append("CREATE TEBLE IF NOT EXISTS Estoque");
+		sql.append("(");
+		sql.append("cod_estoque integer not null,");
+		sql.append("cod_loja integer not null,");
+		sql.append("cod_produto integer not null,");
+		sql.append("quantidade_produto_estoque decimal(10,5),");
+		sql.append("constraint estoque_pkey primary key (cod_estoque),");
+		sql.append("foreign key (cod_loja) references Loja (cod_loja),");
+		sql.append("foreign key (cod_produto) references Produto (cod_produto)");
+		sql.append(")");
+
+		try (Statement comando = link.createStatement();) {
+			comando.execute(sql.toString());
+		} catch (SQLException e) {
+			ConnectPg.printSQLException(e, null);
+		}
+	}
+
+	private void criarCliente(Connection link) {
+		StringBuilder sql = new StringBuilder();
+		sql.append("CREATE TABLE IF NOT EXISTS Cliete");
+		sql.append("(");
+		sql.append("cod_cliente integer not null,");
+		sql.append("cpf_cliente numeric(14) not null,");
+		sql.append("email_cliente varchar(50)");
+		sql.append("telefone_cliente numeric(12)");
+		sql.append("constraint cliente_pkey primary key (cod_cliente)");
+		sql.append(")");
+
+		try (Statement comando = link.createStatement();) {
+			comando.execute(sql.toString());
+		} catch (SQLException e) {
+			ConnectPg.printSQLException(e, null);
+		}
+	}
+
+	private void criarEnderecoCliente(Connection link) {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("CREATE TABLE IF NOT EXISTS Endereco_Cliente");
+		sql.append("(");
+		sql.append("cod_endereco_cliente integer not null,");
+		sql.append("cod_cliente integer not null,");
+		sql.append("nome_rua varchar(60) not null,");
+		sql.append("numero_casa integer not null,");
+		sql.append("cep_cliente numeric(8) not null,");
+		sql.append("complemento_cliente varchar(10),");
+		sql.append("constraint Endereco_Cliente_pkey primary key (cod_endereco_cliente),");
+		sql.append("foreign key (cod_cliente) references Cliente (cod_cliente)");
+		sql.append(")");
+
+		try (Statement comando = link.createStatement();) {
+			comando.execute(sql.toString());
+		} catch (SQLException e) {
+			ConnectPg.printSQLException(e, null);
+		}
+	}
+
+	private void criarFormaPagamento(Connection link) {
+		StringBuilder sql = new StringBuilder();
+		sql.append("CREATE TABLE IF NOT EXISTS Forma_Pagamento");
+		sql.append("(");
+		sql.append("cod_forma_pagamento integer not null,");
+		sql.append("forma_pagamento varchar(50) not null,");
+		sql.append("constraint forma_pagamento_pkey primary key (cod_forma_pagamento),");
+		sql.append(")");
+
+		try (Statement comando = link.createStatement();) {
+			comando.execute(sql.toString());
+		} catch (SQLException e) {
+			ConnectPg.printSQLException(e, null);
+		}
+
+	}
+
+	private void criarVenda(Connection link) {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("CREATE TABLE IF NOT EXISTS Venda");
+		sql.append("(");
+		sql.append("cod_venda integer not null,");
+		sql.append("cod_forma_pagamento integer not null,");
+		sql.append("cod_cliente integer not null,");
+		sql.append("cod_loja integer not null,");
+		sql.append("cod_produto integer not null,");
+		sql.append("valor_compra decimal(10,5),");
+		sql.append("quantidade_produto_venda integer, ");
+		sql.append("constraint venda_pkey primary key (cod_venda),");
+		sql.append("foreign key (cod_forma_pagamento) references Forma_Pagamento (cod_forma_pagamento),");
+		sql.append("foreign key (cod_cliente) references cliente (cod_cliente),");
+		sql.append("foreign key (cod_loja) references loja (cod_loja),");
+		sql.append("foreign key (cod_produto) references produto (cod_produto),");
+		sql.append(")");
+
+		try (Statement comando = link.createStatement();) {
+			comando.execute(sql.toString());
+		} catch (SQLException e) {
+			ConnectPg.printSQLException(e, null);
+		}
 	}
 }
